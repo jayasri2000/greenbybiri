c2ed2ea4d7712fd7b4e803124f9601ec68392b23
jayasri dey <54766450+jayasri2000@users.noreply.github.com>
2021-09-05 21:12:07 +0530

    third commit

---
+++ src/Player.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+
+
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar></Sidebar>
+        <Body></Body>
+      </div>
+     
+    </div>
+  );
+}
+
+export default Player;
